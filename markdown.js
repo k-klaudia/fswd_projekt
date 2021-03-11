e35e7f2f5a1238362b021825adde5114ae3ad16d
sa18b043 <kubovicsklaudia@gmail.com>
2021-03-11 21:15:32 +0100

    error handling added

--- markdown.js
+++ markdown.js
@@ -8,16 +8,19 @@ const parse = require('html-dom-parser');
   
 // Use fs.readFile() method to read the file 
 fs.readFile('text.md', 'utf8', function(err, data){ 
-    var rendered = md.render(data)
-    var result = parse(rendered);
-    // console.log(result[2].children[0].data)
-    result.forEach((item) => {
-    if (item.children !== undefined && item.name !== undefined && item.name === 'h1') {
-        console.log("I am a H1 tag that says: " + item.children[0].data);
-    } else if (item.children !== undefined && item.name !== undefined && item.name === 'p') {
-        console.log("I am a P tag that says: " + item.children[0].data);
+    if(!err){
+        var rendered = md.render(data)
+        var result = parse(rendered);
+        // console.log(result[2].children[0].data)
+        result.forEach((item) => {
+            if (item.children !== undefined && item.name !== undefined && item.name === 'h1') {
+                console.log("I am a H1 tag that says: " + item.children[0].data);
+            } else if (item.children !== undefined && item.name !== undefined && item.name === 'p') {
+                console.log("I am a P tag that says: " + item.children[0].data);
+            }
+            })
     }
-    })
+
       
 }); 
 
