a7f47f804ddea17e3028c5ed9cc5d064e17787cc
sa18b043 <kubovicsklaudia@gmail.com>
2021-03-15 19:50:35 +0100

    renamed

---
+++ PDFUA_converter_app.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+ var exec = require('child_process').exec,
+    child;
+
+rl.question('Would you like to mark the image as an artifact? ', (answer) => {
+  if(answer === 'no') {
+    child = exec('node mdToTaggedPdf.js',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+});
+  } else if (answer === 'yes') {
+    child = exec('node PDFUA_artifact.js',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+});
+  } else {
+    console.log('Pls only enter yes or no thank u')
+  }
+  rl.close();
+});
+
+
